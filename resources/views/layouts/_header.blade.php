ba90c66e610ace61be2a571a8f9af0af0057242e
han <459192435@qq.com>
2019-12-05 13:27:14 +0800

    修改

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -8,6 +8,21 @@
       aria-controls="navbarSupportadContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <!-- Left Side Fo Navbar -->
+      <ul class="navbar-nav mr-auto">
+
+      </ul>
+
+      <!-- Right Side Fo navbar -->
+      <ul class="navbar-nav navbar-rigth">
+        <!-- Authentication links -->
+        <li class="nav-item"><a class="nav-link" href="#">登录</a></li>
+        <li class="nav-itme"><a class="nav-link" href="#">注册</a></li>
+      </ul>
+    </div>
+
   </div>
 
 </nav>
