b243cd9d12da9815af8546ff8d6fc7c630c1be88
han <459192435@qq.com>
2019-12-06 14:49:16 +0800

    邮箱认证

--- app/Http/Middleware/EnsureEmailIsVerified.php
+++ app/Http/Middleware/EnsureEmailIsVerified.php
@@ -20,15 +20,14 @@ class EnsureEmailIsVerified
         // 2. 并且还未认证 Email
         // 3. 并且访问的不是 email 验证相关 URL 或者退出的 URL
 
-        if($request->user() &&
-           !$request->user()->hasVerifiedEmail()&&
-           !$request->is('email/*','logout')){
+        if ($request->user() &&
+            ! $request->user()->hasVerifiedEmail() &&
+            ! $request->is('email/*', 'logout')) {
 
             // 根据客户端返回对应的内容
             return $request->expectsJson()
                 ? abort(403, 'Your email address is not verified.')
                 : redirect()->route('verification.notice');
-
         }
 
         return $next($request);
