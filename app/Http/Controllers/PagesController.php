db2713c5dff0ed4bbc68588d40c2dc40b4749873
han <459192435@qq.com>
2019-12-06 13:56:57 +0800

    修改

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -1,22 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\User;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+
 use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
     public function root()
     {
-        $user = new user();
 
-        if($user instanceof MustVerifyEmail)
-        {
-            return 1;
-        }else{
-            return 0;
-        }
         return view('pages.root');
     }
 }
