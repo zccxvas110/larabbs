0475009ad71015a94a60827c79fd199c1c2cdc7b
han <459192435@qq.com>
2019-12-09 18:00:02 +0800

    修改user文件名

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 
-class UserController extends Controller
+class UsersController extends Controller
 {
     public function show(User $user)
     {
