4fec3a81337c0908ea9f556d32386ffc77bbe492
han <459192435@qq.com>
2019-12-10 18:25:29 +0800

    编辑个人资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -18,8 +18,9 @@ class UsersController extends Controller
         return view('users.edit',compact('user'));
     }
 
-    public function update(UserRequest $request, User $user)
+    public function update(Request $request, User $user)
     {
+
         $user->update($request->all());
         return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
