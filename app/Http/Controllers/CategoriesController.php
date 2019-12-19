3ec49a94699c8e1024e7615369d45dca8e6bfd47
han <459192435@qq.com>
2019-12-19 17:03:44 +0800

    分类话题列表

---
+++ app/Http/Controllers/CategoriesController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use APP\Models\Category;
+
+class CategoriesController extends Controller
+{
+    public function show(Category $category)
+    {
+        $topics = Topic::where('category_id',$category->id)->paginate(20);
+
+        return view('topics.index',compact('topics','category'));
+    }
+}
